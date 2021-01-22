6b043fbdcb97ea91898013b924879c03e49544c7
Belokamenskaya <alexandra.belokamenskaya@gmail.com>
2021-01-23 00:18:46 +0300

    Fix bug index of the right and bottom corners

--- shapeToPNG.py
+++ shapeToPNG.py
@@ -2,15 +2,12 @@ import shapefile
 from PIL import Image
 from PIL import ImageDraw
 import numpy as np
-from min_max_coord import import_shape
-import matplotlib.pyplot as plt
 
 import math
 import shapefile
 
-#нахождение расстояния между точками а,б-широта-долгота
-def length(a1,b1,a2,b2):
-    print('работает length,\n')
+#нахождение расстояния между точками а,б-широта-долгота в МЕТРАХ
+def length(a1, b1, a2, b2):
     a1_r = (a1*math.pi)/180
     a2_r = (a2*math.pi)/180
     b1_r = (b1*math.pi)/180
@@ -25,29 +22,28 @@ def length(a1,b1,a2,b2):
     return length
 
 #импорт шейпа
-def import_shape(file_name,resolution):
-    print('работает pix,\n')    
+def import_shape(file_name,resolution):  
     sf = shapefile.Reader(file_name)
     s = sf.shape(0)
     
-    lon_max=s.points[0][0]
-    lon_min=s.points[0][0]
-    lat_min=s.points[0][1]
-    lat_max=s.points[0][1]
+    lon_max = s.points[0][0]
+    lon_min = s.points[0][0]
+    lat_min = s.points[0][1]
+    lat_max = s.points[0][1]
     #мин макс координаты
     for i in range(0,len(s.points)):
         
         if s.points[i][0] > lon_max:
-            lon_max=s.points[i][0]
+            lon_max = s.points[i][0]
             
         if s.points[i][0] < lon_min:
-            lon_min=s.points[i][0] 
+            lon_min = s.points[i][0] 
             
         if s.points[i][1] < lat_min:
-            lat_min=s.points[i][1]
+            lat_min = s.points[i][1]
             
         if s.points[i][1] > lat_max:
-            lat_max=s.points[i][1]  
+            lat_max = s.points[i][1]  
             
     # print(lon_max,"\n")
     # print(lon_min,"\n")
@@ -58,18 +54,20 @@ def import_shape(file_name,resolution):
     lon_length = length(0, lon_min, 0, lon_max)
     lat_length = length(lat_min, 0, lat_max, 0)
     
-    print( ' Length Longitude',lon_length,"\n")
-    print( ' Length Latitude',lat_length,"\n")
+    print( 'shape width in meters',  lon_length)
+    print( 'shape height in meters', lat_length,"\n")
     
-    print( ' lon_min',lon_min,"\n")
-    print( ' lon_max',lon_max,"\n")
-    print( ' lat_min',lat_min,"\n")
-    print( ' lat_max',lat_max,"\n")
+    # print( ' lon_min',lon_min)
+    # print( ' lon_max',lon_max)
+    # print( ' lat_min',lat_min)
+    # print( ' lat_max',lat_max)
     
     width = math.ceil(lon_length/resolution) 
     height = math.ceil(lat_length/resolution)
     
     # print(width, height,"\n")
+    print( 'shape width in pixels',  width)
+    print( 'shape height in pixels', height,"\n")
 
     return(width, height, lon_min, lon_max, lat_min, lat_max)
 
@@ -78,8 +76,7 @@ def import_shape(file_name,resolution):
 # width,height,lat_max,lon_min=import_shape(r'E:\districts\two\two.shp')
 # print(width,height,lat_max,lon_min)
 
-def shapeToPNG(file_name, width, height, res_folder, name_png):
-    print('работает shape_png,\n')  
+def shapeToPNG(file_name, width, height, res_folder, name_png):  
     # Read in a shapefile
     r = shapefile.Reader(file_name)
     # Geographic x & y distance
@@ -102,8 +99,7 @@ def shapeToPNG(file_name, width, height, res_folder, name_png):
     img.save(res_folder + name_png + '.png')
     img = Image.open(res_folder + name_png + '.png')
  
-    arr = np.asarray(img, dtype = 'uint8')
-    print(arr.shape)  
+    arr = np.asarray(img, dtype = 'uint8') 
   
     return arr
 
